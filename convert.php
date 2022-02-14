d657c1a68173e3b9d0a74cf6618ca716e54ee5d1
p44797jb <jiahang.bian@student.manchester.ac.uk>
2022-02-14 16:49:21 +0000

    Testing php

---
+++ convert.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Member check</title>
+ </head>
+ <body>
+  <h1>Member check</h1>
+  <?php
+    $c = $_GET['name'];
+    echo ( " Hi " . $c . " welcome to X9 Group Project");
+  ?>
+ </body>
+</html>
